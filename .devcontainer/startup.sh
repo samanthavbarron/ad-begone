def7eb1e7c70981948a5e4d0c090a92d45de9370
Samantha Barron <sam@sambarron.me>
2025-02-16 16:00:46 +0000

    init

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv python install 3.12
---
+++ test/test_import.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+class TestImport(TestCase):
+
+    def test_import(self):
+        import ad_begone
